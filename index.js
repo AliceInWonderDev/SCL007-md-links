f79252eafc554d27447fa27955add80ec702d5a1
AliceInWonderDev <ali.castaneda05@gmail.com>
2019-02-27 16:40:39 -0300

    intentando promesas

--- index.js
+++ index.js
@@ -1,4 +1,119 @@
 #!/usr/bin/env node
+let fs = require('fs');
+let fetch = require('node-fetch');
+let path = require('path');
+let anyDocument = process.argv[2];
+let markdownLinkExtractor = require('markdown-link-extractor');
+readline = require('readline');
+let pathRuta = "";
+
+let mdLinks = (pathRuta) =>{
+    pathRuta = path.resolve(anyDocument);
+    let markdown = fs.readFile(pathRuta).toString();
+    let link = markdownLinkExtractor(markdown);
+    let arregloLinks = [];
+    //recorrer lineas
+    for(let i=0; i < link.length; i++){
+        const url= link[i];
+
+    //mostrar cuales estan rotos
+    let arregloFetch = fetch(link[i])
+        .then((res)=>{
+            let infoLinks = {
+                url:`${res.url}`,
+                status:`${res.status}`,
+                text: `${res.text}`
+            }
+            return infoLinks;
+        })
+        .catch((err)=>{
+            let falla = {
+                urlLink: `${url}`,
+                satusLink:"error",
+            }
+        })
+        result.push(arregloFetch);
+    }
+    Promise.all(arregloLinks)
+        .then((lineaLinks)=>{
+            console.log(lineaLinks)
+        })
+        .catch(console.error)
+};
+mdLinks();
+//-------------------------------------------------------
+
+// let markdown = fs.readFileSync(anyDocument).toString();
+// let link = markdownLinkExtractor(markdown);
+// link.forEach(function (link) {
+//     console.log(link);
+// });
+
+// for(let i=0; i < link.length; i++){
+//     if(link === true){
+    
+//     }
+
+// }
+
+
+//-------------------------------------------------------
+
+
+
+
+
+
+// let mdLinks = {}
+
+// mdLinks.readDirectory = (anyDocument) => {
+
+//     fs.readdir(anyDocument, function(error, list){
+//         if(error){
+//             console.log(error);
+//         }
+//         list.forEach(function(files){
+//             if(fs.lstatSync(files).isDirectory()){
+//                 return mdLinks.readDirectory(files);
+//             }
+//             if(fs.lstatSync(files).isFile()){
+//                 return mdLinks.readFile(files);
+//             }
+//             console.log(files);
+//         })
+//     })
+
+// }
+
+// mdLinks.readFile = (anyDocument) => {
+//     fs.readFile(anyDocument, function(error, file){
+//         if (error){
+//             console.log(error);
+//         }
+//         file.forEach(function(files){
+
+//         })
+//     })
+// }
+
+
+// function getData(anyDocument, type) {
+//     return new Promise(function(resolve, reject){
+//       fs.readFile(anyDocument, type, (err, data) => {
+//           err ? reject(err) : resolve(data);
+//       });
+//     });
+    
+//   } 
+// var rd = readline.createInterface({
+//     input: fs.createReadStream('anyDocument'),
+//     output: process.stdout,
+//     console: false
+// });
+
+// rd.on('line', function(line) {
+//     console.log(line);
+// });
 
 //  let fs = require('fs');
 //  let path = require('path');
@@ -15,12 +130,6 @@
 //   console.log(data);
 // });
 
-let fs = require('fs');
-let fetch = require('node-fetch');
-let path = require('path');
-let anyDocument = process.argv[2];
-readline = require('readline');
-
 
 // if (dirEntry.isDirectory === true){
 // // See the section on FileEntry for other inherited properties/methods.
@@ -31,28 +140,3 @@ readline = require('readline');
 // }else{
 //     console.log(error);
 // }
-
-
-
-    let markdownLinkExtractor = require('markdown-link-extractor');
-
-    var markdown = fs.readFileSync(anyDocument).toString();
-
-    let link = markdownLinkExtractor(markdown);
-
-    link.forEach(function (link) {
-        console.log(link);
-    });
-    
-
-
-
-var rd = readline.createInterface({
-    input: fs.createReadStream('anyDocument'),
-    output: process.stdout,
-    console: false
-});
-
-rd.on('line', function(line) {
-    console.log(line);
-});
